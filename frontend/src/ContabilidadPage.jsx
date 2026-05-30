5049766eecd455e33f15df2084a47926000817f4
domus_desarrollo2 <desarrollo2@domus.la>
2026-05-30 14:39:04 -0500

    new

--- frontend/src/ContabilidadPage.jsx
+++ frontend/src/ContabilidadPage.jsx
@@ -287,8 +287,6 @@ function ContabilidadPage({ user }) {
                 </div>
             </div>
         </div>
-
-
         
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 mt-2">
             <div className="rounded-3xl border border-slate-200 bg-white p-6 shadow-sm dark:border-slate-800 dark:bg-slate-900">
